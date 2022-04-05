b816a7a936fef0437fbaa54528e80a2a1e591ea5
truongnq00 <truongnq00@gmail.com>
2022-04-05 16:03:39 +0700

    Setup and lab 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('products')->name('products.')->group(function ()
+{
+    Route::get('/', [ProductController::class, 'index'])->name('index');
+    Route::delete('/{id}', [ProductController::class, 'destroy'])->name('destroy');
+});
+
